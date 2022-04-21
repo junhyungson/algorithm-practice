bdd74eedd8b7b9a108fcd6be57209fbc7cbe70a8
junhyungson <35462792+junhyungson@users.noreply.github.com>
2022-04-21 09:04:42 -0700

    Initial commit

---
+++ colt/frequencyCounter.js
@@ -0,0 +1,11 @@
+export function sameFrequency(n1, n2) {
+  // return false if arr1 and arr2 is a negative value
+  if (n1 && n2 < 0) return false;
+
+  let n1toString = n1.toString();
+  let n2toString = toString(n2);
+  let frequency1 = {};
+  let frequency2 = {};
+
+  console.log(n1toString);
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles.css";
+import { sameFrequency } from "../colt/index";
+
+console.log(sameFrequency(182, 281));
